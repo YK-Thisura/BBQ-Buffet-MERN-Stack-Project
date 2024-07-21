8a14047651ca6e5676be87517905f08b25984cd7
Theekshana Thisura <yktheekshanathisura@gmail.com>
2024-07-21 10:59:42 +0530

    Revert "Set Database to Backend"
    This reverts commit bbb220ae7a73a2bac4650ffba82d5e9c97484656.

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://thisura:thisura11@cluster0.mrfih2p.mongodb.net/Restaurant Web Application"
-    )
-    .then(() => console.log("DB Connected"));
-};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { connectDB } from "./config/db.js";
 
 //app config
 
@@ -12,11 +11,14 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
+<<<<<<< HEAD
 //db connection
 connectDB();
 
 // api  endpoint
 
+=======
+>>>>>>> parent of bbb220a (Set Database to Backend)
 app.get("/", (req, res) => {
   res.send("API Working");
 });
