e717af7878e8e301ed948a51a0eb431a3dd6547c
Theekshana Thisura <yktheekshanathisura@gmail.com>
2024-07-21 11:10:30 +0530

    Revert "Dev food Routes"
    This reverts commit 427bce837ba196cbc27883a44231c7746d65d9bc.

--- backend/routes/foodRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { addFood } from "../controllers/foodController.js";
-import multer from "multer";
-
-const foodRouter = express.Router();
-
-export default foodRouter;
